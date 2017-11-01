8417daca01b08383f73520c85f03dba73ba71318
hellerve <veit@veitheller.de>
2017-11-01 20:41:29 +0100

    internatioanlization

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var fs = require('fs');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index.html');
 });
@@ -10,6 +9,15 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
   fs.appendFile("./people", JSON.stringify(req.body));
   res.render('success.html');
+});
+
+router.get('/de', function(req, res, next) {
+  res.render('de.html');
+});
+
+router.post('/de', function(req, res, next) {
+  fs.appendFile("./people", JSON.stringify(req.body));
+  res.render('success.de.html');
 });
 
 module.exports = router;
